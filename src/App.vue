7f1ed4c754ad88f0de87605becbae52932cf2e29
123 <906084627@qq.com>
2018-04-10 22:22:59 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <header></header>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+
+import Header from './components/Header'
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+
+</style>
